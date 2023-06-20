8138597179a4ce1da35db53fe5711494594a34ac
Mohammad Arkan <a.r.khan.coxyboys@gmail.com>
2023-06-20 12:15:34 +0600

    added readme.md file

--- src/componenst/Home/Home.jsx
+++ src/componenst/Home/Home.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Home.css'
 import { useLoaderData } from 'react-router-dom';
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
 
 const Home = () => {
-    const tshirtData = useLoaderData();
-    console.log(tshirtData)
+    const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+    const handleAddToCart = tshirt => {
+        console.log(tshirt)
+    }
+    console.log(tshirts)
     return (
-        <div>
-            <h1>this is Home page: {tshirtData.length}</h1>
+        <div className='home-container'>
+          <div className='t-shirts-container'>
+            
+            {
+                tshirts.map(tshirt => <TShirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                handleAddToCart={handleAddToCart}
+                ></TShirt> )
+            }
+          </div>
+          <div className='cart-container'>
+            <Cart></Cart>
+          </div>
         </div>
+    
     );
 };
 
